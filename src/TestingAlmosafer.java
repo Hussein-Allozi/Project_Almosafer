172699088fa0abce08bfcd05f0927f919690f827
Hussein-Allozi <allozihussein@gmail.com>
2024-08-16 13:10:48 -0700

    update

--- src/TestingAlmosafer.java
+++ src/TestingAlmosafer.java
@@ -13,15 +13,15 @@ import org.testng.annotations.Test;
 
 public class TestingAlmosafer {
 	WebDriver driver = new ChromeDriver();
-
+	Random rand = new Random();
 	@BeforeTest
 	public void TheFirst() {
 
 		driver.get("https://global.almosafer.com/en");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.findElement(By.cssSelector("button[class='sc-jTzLTM hQpNle cta__button cta__saudi btn btn-primary']"))
-				.click();
+		driver.findElement(By.cssSelector("button[class='sc-jTzLTM hQpNle cta__button cta__saudi btn btn-primary']")).click();
+		
 
 	}
 
@@ -85,6 +85,32 @@ public class TestingAlmosafer {
 		Assert.assertEquals(Tomorow, dep);
 		Assert.assertEquals(AfterTomorow, ari);
 
+	}
+	@Test(priority = 7)
+	public void RandomLanguageCheck() {
+		String [] lang = {"https://global.almosafer.com/en","https://global.almosafer.com/ar"};
+		int RandomLanguage = rand.nextInt(lang.length);
+		driver.get(lang[RandomLanguage]);
+		String Url= driver.findElement(By.tagName("link")).getAttribute("href");
+		Assert.assertEquals(lang[RandomLanguage], Url);
+//		System.out.println(Url);
+	}
+	@Test(priority = 8)
+	public void HotelSearchTab() {
+		WebElement  HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		HotelTab.click();
+		String language = driver.findElement(By.tagName("html")).getAttribute("lang");
+		WebElement SearchInput = driver.findElement(By.xpath("//input[@data-testid='AutoCompleteInput']"));
+		if(language.equals("en")) {
+		SearchInput.sendKeys("Dubai , jeddah , Riyadh");
+	}
+		else {
+			SearchInput.sendKeys(" دبي , جدة ");
+		}
+		List<WebElement> FirstResult = driver.findElements(By.xpath("//ul[@data-testid='AutoCompleteResultsList']"));
+		FirstResult.get(0).click();
+		WebElement ButtonSearch = driver.findElement(By.xpath("//button[@data-testid='HotelSearchBox__SearchButton']"));
+		ButtonSearch.click();
 	}
 
 }
